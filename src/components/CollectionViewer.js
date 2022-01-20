2878b6ca7810332edc99999c05da8fd9c5cc0c0b
readinggaol <dougparker.m@gmail.com>
2022-01-20 17:51:20 -0330

    Image fetching working
    Image fetching now works

--- src/components/CollectionViewer.js
+++ src/components/CollectionViewer.js
@@ -2,15 +2,28 @@ import { useState, useEffect } from "react"
 
 const CollectionViewer = (props) => {
     
+    let scryURL = "https://api.scryfall.com/cards/named?exact="
+    let scryImageURL = "https://api.scryfall.com/cards/"
+
+    const [currentCard, setCurrentCard] = useState()
+
+    //Get the name of the current card, parse it, then fetch the image and update state
+    const updateCard = async (e) => {
+        let cardURL = scryURL + e.currentTarget.value.replaceAll(" ", "+")
+        let res = await fetch(cardURL)
+        let data = await res.json()
+        let imageURL = scryImageURL + data.id + "?format=image"
+        setCurrentCard(imageURL)
+    }
   
     return (
         <div className="collectionviewer">
             <p>{props.label}</p>
-            <select className="form-control">
+            <select className="form-control" onChange={updateCard}>
                 {props.cards.map((card) => (<option key={card.card_id}>{card.name}</option>) )}
             </select>
-            <label>( {props.cards.length} unique )</label>
-            <img></img>
+            <p>( {props.cards.length} unique )</p>
+            <img src={currentCard}></img>
         </div>
     )
 }
